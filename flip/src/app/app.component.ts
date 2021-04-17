2d0cbd0a5ef9e7e0f17800342c4667a20b1e22bb
SiddharthJoshi1 <siddharthjoshi1@hotmail.com>
2021-04-17 23:08:17 +0100

    Added functionality to make images appear on click

--- flip/src/app/app.component.ts
+++ flip/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import * as cards from './cardslist.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'flip';
+  cards = (cards as any).default;
+  CardImg1 : string ='';
+  CardImg2 : string ='';
+  CardID1 = '';
+  CardID2 = '';
+
+  onCardClicked(event: any): void{
+    event.target.querySelector('img').id
+    event.target.querySelector('img').hidden = false
+
+    if(this.CardImg1.length > 1){
+      this.CardImg2 = event.target.querySelector('img').src
+      this.CardID2 = event.target.querySelector('img').id
+    }else{
+      this.CardImg1 = event.target.querySelector('img').src
+      this.CardID1 = event.target.querySelector('img').id
+    }
+
+    if(this.CardImg1 !== this.CardImg2 && this.CardImg1.length >1 && this.CardID2.length > 1){
+      if(this.CardID1 === this.CardID2){
+        
+      }
+    }
+
+  }
 }
