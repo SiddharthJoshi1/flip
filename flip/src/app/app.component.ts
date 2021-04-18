1e9cb276818b44567e68327d273e9ed234315b55
SiddharthJoshi1 <siddharthjoshi1@hotmail.com>
2021-04-18 22:32:17 +0100

    added removal functionality

--- flip/src/app/app.component.ts
+++ flip/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { identifierModuleUrl } from '@angular/compiler';
 import { Component } from '@angular/core';
 import * as cards from './cardslist.json';
 
@@ -14,24 +15,39 @@ export class AppComponent {
   CardImg2 : string ='';
   CardID1 = '';
   CardID2 = '';
+  Card1IsSet = false;
+  Card2IsSet = false;
 
   onCardClicked(event: any): void{
     event.target.querySelector('img').id
     event.target.querySelector('img').hidden = false
 
-    if(this.CardImg1.length > 1){
+    if(this.Card2IsSet){
+      this.CardImg1 = event.target.querySelector('img').src
+      this.CardID1 = event.target.querySelector('img').id
+      this.Card1IsSet = true
+      this.Card2IsSet = false
+    }else if(this.Card1IsSet){
       this.CardImg2 = event.target.querySelector('img').src
       this.CardID2 = event.target.querySelector('img').id
+      this.Card2IsSet = true
+      this.Card1IsSet = false
     }else{
       this.CardImg1 = event.target.querySelector('img').src
       this.CardID1 = event.target.querySelector('img').id
+      this.Card1IsSet = true
     }
 
-    if(this.CardImg1 !== this.CardImg2 && this.CardImg1.length >1 && this.CardID2.length > 1){
-      if(this.CardID1 === this.CardID2){
-        
-      }
-    }
+    console.log(this.CardID1)
+    console.log(this.CardID2)
+    console.log(this.CardImg1)
+    console.log(this.CardImg2)
 
+    if(this.CardImg1 === this.CardImg2){
+      document.getElementById(this.CardID1)?.parentElement?.parentElement?.remove()
+      document.getElementById(this.CardID2)?.parentElement?.parentElement?.remove()
+    }else{
+      console.log("hidden")
+    }
   }
 }
